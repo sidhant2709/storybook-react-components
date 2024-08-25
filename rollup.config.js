0ef088237824459c9310d3d5d6fe20394698b3d5
Sidhant <sidhant1593@gmail.com>
2024-08-25 23:09:04 +0530

    feat: automated release implemented

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs", // Output as CommonJS module.
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm", // Output as ES module.
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json", jsx: "react" }),
+      terser(),
+      postcss(),
+    ],
+    external: ["react", "react-dom"], // Excludes specified dependencies from the bundle.
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: packageJson.types, format: "esm" }],
+    plugins: [dts.default()],
+    external: [/\.css$/],
+  },
+];
