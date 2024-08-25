7d5d20feeb11dbb08dfccb5d2ebc43c7ca02d0da
Sidhant <sidhant1593@gmail.com>
2024-08-25 17:00:19 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Button from "./Button/Button";
+import Header from "./Header/Header";
+
+import type { ButtonProps } from "./Button/Button";
+import type { HeaderProps, User } from "./Header/Header";
+
+export { Button, ButtonProps, Header, HeaderProps, User };
